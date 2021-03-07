d8c20cb1ff6b0385a2cd1ed2d0c46e21e382b02b
Mark <mark.duplock@gmail.com>
2021-03-07 16:49:51 +0000

    Create main.py

---
+++ main.py
@@ -0,0 +1,14 @@
+#Write your code below this row 👇
+
+fizz = "Fizz"
+buzz = "Buzz"
+
+for number in range (1, 101):
+  if number % 3 == 0 and number % 5 == 0:
+    print(fizz + buzz)
+  elif number % 5 == 0:
+    print(buzz)
+  elif number % 3 == 0:
+    print(fizz)
+  else:
+    print(number)
